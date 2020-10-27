7a9c4d823992542dc4519f6a21259eb7a2f80c11
Sebastian <sebastian.garciagasco@gmail.com>
2020-10-27 16:38:32 +0100

    Finalize pbc.py
    - Fixed case of negative coordinates
    - Added minimum_image() function

--- Exercise 1/pbc.py
+++ Exercise 1/pbc.py
@@ -1,13 +1,26 @@
-import math as m
+"""
+CMod Exercise 1: A collection of functions for use in 
+Periodic Boundary Condition problems for a cube of length 'l'
 
-def pbc(x,l):
-  return [i%l for i in x]
+Author: Sebastian Garcia
+Version: 10/2020
+"""
 
-def mic(x,l):
-  x = pbc(x,l)
-  corners = [[0,0,0],[0,0,l],[0,l,0],[l,0,0],[0,l,l],[l,0,l],[l,l,0],[l,l,l]]
-  mic = l
-  for corner in corners:
-    mic = min(mic,m.sqrt(sum([(i-j)**2 for i,j in zip(x,corner)])))
-  return mic
+def periodic_image(x:list,l:float) -> list:
+  """
+  Periodic Boundary
+  :param x: point in R^3 
+  :param l: cube length 
+  :return: image of the point inside the 'original' cube 
+  """
+  return [i % l if i >= 0 else (l-(i % l)) % l for i in x]
 
+def minimum_image(x:list,l:float) -> list:
+  """
+  Minimum Image Convention
+  :param x: point in R^3
+  :param l: cube length
+  :return: image of the point closest to the origin
+  """
+  x = periodic_image(x,l)
+  return [i if i <= l/2 else i-l for i in x]
